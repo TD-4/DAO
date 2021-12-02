488f94c4723864e50372ff582c0fc3be3ea9bf96
felixfu520 <alanmathisonturing@163.com>
2021-12-02 11:32:18 +0800

    trainval, eval, demo, export

---
+++ core/tools/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @auther:FelixFu
+# @Date: 2021.10.1
+# @github:https://github.com/felixfu520
+
+from .utils import register_modules
+
+from .trainval import TrainVal
