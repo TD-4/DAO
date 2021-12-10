4c8e929c3fb65901a22d0b04348369d4f670f017
felixfu520 <alanmathisonturing@163.com>
2021-12-10 17:13:40 +0800

    trainers

--- core/trainers/__init__.py
+++ core/trainers/__init__.py
@@ -1,7 +1,5 @@
 from .launch import launch
 from .trainerCls import ClsTrainer, ClsDemo, ClsEval, ClsExport
 from .trainerSeg import SegTrainer, SegDemo, SegEval, SegExport
-# from .trainerDet import DetTrainer
-# from .trainerSeg import SegTrainer
-# from .trainerIQA import IQATrainer
+from .trainerDet import DetTrainer, DetDemo, DetEval, DetExport
 
