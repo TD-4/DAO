5b6c118031b1ac4f6dffcfcf8c395f3fd3fd8905
felixfu520 <alanmathisonturing@163.com>
2021-12-24 17:13:48 +0800

    @ Seg trainer

--- core/trainers/trainerSeg.py
+++ core/trainers/trainerSeg.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Copyright (c) Megvii, Inc. and its affiliates.
+
+# -*- coding: utf-8 -*-
+# @Author:FelixFu
+# @Date: 2021.12.17
+# @GitHub:https://github.com/felixfu520
+# @Copy From:
 
 import os
 import time
@@ -10,14 +13,15 @@ from PIL import Image
 from loguru import logger
 
 import torch
+from torchsummary import summary
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter
-from torchsummary import summary
 
 from core.modules import Registers
 from core.utils import get_rank, get_local_rank, get_world_size, all_reduce_norm, synchronize
 from core.trainers.utils import (
-    setup_logger, load_ckpt, save_checkpoint, occupy_mem, ModelEMA, is_parallel, gpu_mem_usage,
+    setup_logger, load_ckpt, save_checkpoint, occupy_mem,
+    ModelEMA, EMA, is_parallel, gpu_mem_usage,
     get_palette, colorize_mask
 )
 from core.modules.utils import MeterSegTrain
@@ -57,7 +61,7 @@ class SegTrainer:
         8.Evaluator Setting;
         """
         self.output_dir = os.getcwd() if self.exp.trainer.log.log_dir is None else \
-            os.path.join(self.exp.trainer.log.log_dir, self.exp.name, self.start_time)
+            os.path.join(self.exp.trainer.log.log_dir, self.exp.name)   # , self.start_time
         setup_logger(self.output_dir, distributed_rank=get_rank(), filename=f"train_log.txt", mode="a")
         logger.info("....... Train Before, Setting something ...... ")
         logger.info("1. Logging Setting ...")
@@ -69,9 +73,9 @@ class SegTrainer:
 
         logger.info("2. Model Setting ...")
         torch.cuda.set_device(get_local_rank())
-        model = Registers.seg_models.get(self.exp.model.type)(**self.exp.model.kwargs)  # get model from register
+        model = Registers.seg_models.get(self.exp.model.type)(self.exp.model.backbone, **self.exp.model.kwargs)
         logger.info("\n{}".format(model))   # log model structure
-        summary(model, input_size=tuple(self.exp.model.summary_size), device="{}".format(next(model.parameters()).device))  # log torchsummary model
+        summary(model, input_size=tuple(self.exp.model.summary_size), device="{}".format(next(model.parameters()).device))
         model.to("cuda:{}".format(get_local_rank()))    # model to self.device
 
         logger.info("3. Optimizer Setting")
@@ -116,13 +120,15 @@ class SegTrainer:
         # 用EMA方法对模型的参数做平均，以提高测试指标并增加模型鲁棒性（减少模型权重抖动）
         self.use_model_ema = self.exp.trainer.ema
         if self.use_model_ema:
-            self.ema_model = ModelEMA(model, 0.9998)
-            self.ema_model.updates = self.max_iter * self.start_epoch
+            # self.ema_model = ModelEMA(model, 0.9998)
+            # self.ema_model.updates = self.max_iter * self.start_epoch
+            self.ema_model = EMA(model, 0.9998)
+            self.ema_model.register()
 
         self.model = model
         self.model.train()
 
-        logger.info("8. Evaluator Setting ... ")
+        logger.info("9. Evaluator Setting ... ")
         self.evaluator = Registers.evaluators.get(self.exp.evaluator.type)(
             is_distributed=get_world_size() > 1,
             dataloader=self.exp.evaluator.dataloader,
@@ -134,10 +140,10 @@ class SegTrainer:
 
     def _before_epoch(self):
         """
-        判断此次epoch是否需要马赛克增强
+        每次epoch前的操作，例如multi-scale， 马赛克增强，等取消与否。
         :return:
         """
-        logger.info("---> start train epoch{}".format(self.epoch + 1))
+        logger.info("------------------> start train epoch{}".format(self.epoch + 1))
 
     def _before_iter(self):
         pass
@@ -153,10 +159,10 @@ class SegTrainer:
 
         with torch.cuda.amp.autocast(enabled=self.exp.trainer.amp):    # 开启auto cast的context manager语义（model+loss）
             outputs = self.model(inps)
-            loss = self.loss(outputs[0], targets)  # PSP损失
-            loss += self.loss(outputs[1], targets) * 0.4  # FCN损失
-            outputs = outputs[0]
-            # loss = self.loss(outputs, targets)
+            # loss = self.loss(outputs[0], targets)  # PSP损失
+            # loss += self.loss(outputs[1], targets) * 0.4  # FCN损失
+            # outputs = outputs[0]
+            loss = self.loss(outputs, targets)
 
         self.optimizer.zero_grad()   # 梯度清零
         self.scaler.scale(loss).backward()   # 反向传播；Scales loss. 为了梯度放大
@@ -167,8 +173,8 @@ class SegTrainer:
         self.scaler.update()    # 准备着，看是否要增大scaler
 
         if self.use_model_ema:
-            self.ema_model.update(self.model)
-
+            # self.ema_model.update(self.model)
+            self.ema_model.update()
         lr = self.lr_scheduler.update_lr(self.progress_in_iter + 1)
         for param_group in self.optimizer.param_groups:
             param_group["lr"] = lr
@@ -220,26 +226,26 @@ class SegTrainer:
             self._evaluate_and_save_model()
 
     def _after_train(self):
-        logger.info(
-            "Training of experiment is done and the best Acc is {:.2f}".format(self.best_acc)
-        )
+        logger.info("Training of experiment is done and the best Acc is {:.2f}".format(self.best_acc))
 
     def _evaluate_and_save_model(self):
         if self.use_model_ema:
-            evalmodel = self.ema_model.ema
+            # evalmodel = self.ema_model.ema
+            evalmodel = self.ema_model.model
         else:
             evalmodel = self.model
             if is_parallel(evalmodel):
                 evalmodel = evalmodel.module
 
-        pixAcc, mIoU, Class_IoU = self.evaluator.evaluate(evalmodel, get_world_size() > 1,
-                                        device="cuda:{}".format(get_local_rank()))
+        pixAcc, mIoU, Class_IoU = self.evaluator.evaluate(evalmodel, get_world_size() > 1, device="cuda:{}".format(get_local_rank()))
         self.model.train()
+        logger.info("pixAcc:{}, mIoU:{}, Class_IoU:{}".format(pixAcc, mIoU, Class_IoU))
+
         if get_rank() == 0:
             self.tblogger.add_scalar("val/pixAcc", pixAcc, self.epoch + 1)
             self.tblogger.add_scalar("val/mIoU", mIoU, self.epoch + 1)
             for k, v in Class_IoU.items():
-                self.tblogger.add_scalar("val/Class_IoU", v, self.epoch + 1)
+                self.tblogger.add_scalar("val/{} IoU".format(k), v, self.epoch + 1)
 
         synchronize()
         self._save_ckpt("last_epoch", mIoU > self.best_acc)
@@ -247,8 +253,9 @@ class SegTrainer:
 
     def _save_ckpt(self, ckpt_name, update_best_ckpt=False):
         if get_rank() == 0:
-            save_model = self.ema_model.ema if self.use_model_ema else self.model
-            logger.info("Save weights to {}".format(self.output_dir))
+            # save_model = self.ema_model.ema if self.use_model_ema else self.model
+            save_model = self.ema_model.model if self.use_model_ema else self.model
+            logger.info("Save weights to {} - update_best_ckpt:{}".format(self.output_dir, update_best_ckpt))
             ckpt_state = {
                 "start_epoch": self.epoch + 1,
                 "model": save_model.state_dict(),
