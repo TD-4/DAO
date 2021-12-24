03ee7d4edc6e6a331e2a40da029ab28f5092107b
felixfu520 <alanmathisonturing@163.com>
2021-12-24 17:06:19 +0800

    @ SegEvaluator 添加labels_dict

--- core/modules/evaluators/SegEvaluator.py
+++ core/modules/evaluators/SegEvaluator.py
@@ -2,13 +2,7 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii, Inc. and its affiliates.
 
-import contextlib
-import io
-import itertools
-import json
-import tempfile
-import time
-from loguru import logger
+
 from tqdm import tqdm
 
 import torch
@@ -20,12 +14,7 @@ from core.modules.utils import MeterSegEval
 
 @Registers.evaluators.register
 class SegEvaluator:
-    def __init__(
-            self,
-            is_distributed=False,
-            dataloader=None,
-            num_classes=None,
-    ):
+    def __init__(self, is_distributed=False, dataloader=None, num_classes=None):
         self.dataloader, self.iters_per_epoch = Registers.dataloaders.get(dataloader.type)(
             is_distributed=is_distributed,
             dataset=dataloader.dataset,
@@ -39,34 +28,37 @@ class SegEvaluator:
         model = model.eval()
         if half:
             model = model.half()
-        data_list = []
+        data_list = []  # 用于存放all world size汇集的数据
 
         progress_bar = tqdm if is_main_process() else iter
 
         self.meter.reset_metrics()
-        for cur_iter, (imgs, targets, paths) in enumerate(progress_bar(self.dataloader)):
+        for imgs, targets, paths in progress_bar(self.dataloader):
             with torch.no_grad():
                 imgs = imgs.to(device=device)
                 targets = targets.to(device=device)
                 imgs = imgs.type(tensor_type)
                 outputs = model(imgs)
-                data_list.append((outputs, targets, paths))
-        if distributed:
-            output_s = []
+                data_list.append((outputs, targets, paths)) # data_list:[([32, 21, 224, 224], [32, 21, 224, 224],[(image_p,...),(mask_p,...)]), ...]
+
+        if distributed:  # 如果是分布式，将结果gather到0设备上
+            output_s = []   #
             target_s = []
             path_s = []
             data_list = gather(data_list, dst=0)
+            # data_list [[([32, 21, 224, 224], [32, 21, 224, 224],[(image_p,...),(mask_p,...)]), ...], ..., N_gpus]
             for data_ in data_list:     # multi gpu
-                for pred, target in data_:  # 每个gpu所具有的sample
-                    output_s.append(pred)
-                    target_s.append(target)
+                for pred, target, path in data_:  # 每个gpu所具有的sample
+                    output_s.append(pred)  # [(16, 21, 224, 224), ...] 16为设定batchsize/ngpus
+                    target_s.append(target)  # [(16, 224, 224), ...]
+                    path_s.append(path)  # [(16个path, 16个path), ...]
         else:
             output_s = []
             target_s = []
             path_s = []
-            for o, t, p in data_list:     # data_list [batchsize, batchsize,...]
-                output_s.append(o)  # torch.Size([6, 21, 224, 224])
-                target_s.append(t)  # torch.Size([6, 224, 224])
+            for o, t, p in data_list:  # data_list:[([32, 21, 224, 224], [32, 21, 224, 224],[(image_p,...),(mask_p,...)]), ...]
+                output_s.append(o)  # [[6, 21, 224, 224], ...]
+                target_s.append(t)  # [[6, 21, 224, 224], ...]
                 path_s.append(p)    # list[list of jpg, list of png]
 
         if not is_main_process():
@@ -77,4 +69,7 @@ class SegEvaluator:
                 self.meter.update_seg_metrics(*seg_metrics)
         # PRINT INFO
         pixAcc, mIoU, Class_IoU = self.meter.get_seg_metrics().values()
-        return pixAcc, mIoU, Class_IoU
+        Class_IoU_dict = {}
+        for k, v in Class_IoU.items():
+            Class_IoU_dict[self.dataloader.dataset.labels_dict[str(k)]] = v
+        return pixAcc, mIoU, Class_IoU_dict
