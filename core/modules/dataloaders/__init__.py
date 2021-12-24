57ccc70bfecad1ba0d527ba14d63830116b7e5e1
felixfu520 <alanmathisonturing@163.com>
2021-12-24 17:05:14 +0800

    @ dataloader

--- core/modules/dataloaders/__init__.py
+++ core/modules/dataloaders/__init__.py
@@ -4,7 +4,14 @@
 # @GitHub:https://github.com/felixfu520
 # @Copy From:
 
+# dataset
 from .datasets import MVTecDataset, ClsDataset, DetDataset, SegDataset, IQADataset
 
+# anomaly
 from .AnomalyDataloader import MVTecDataloader
 
+# classification
+from .ClsDataloader import ClsDataloaderTrain, ClsDataloaderEval
+
+# segmentation
+from .SegDataloader import SegDataloaderTrain, SegDataloaderEval
