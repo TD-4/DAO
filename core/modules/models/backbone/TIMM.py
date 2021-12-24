1011bd1afea20519991a91105113603a3ceb393b
felixfu520 <alanmathisonturing@163.com>
2021-12-24 17:10:48 +0800

    @ fixed version error, add raise

--- core/modules/models/backbone/TIMM.py
+++ core/modules/models/backbone/TIMM.py
@@ -16,14 +16,16 @@ def TIMM(backbone):
     # 判断model是否在timm支持列表中
     if backbone.kwargs.model_name not in timm.list_models():
         logger.error("timm {} not supported {}".format(
-            timm.__version,
+            timm.__version__,
             backbone.kwargs.model_name))
+        raise
 
     # 判断model是否有pretrained
     if backbone.kwargs.pretrained and backbone.kwargs.model_name not in timm.list_models(pretrained=True):
         logger.error("{} hasn't pretrained weight, please set pretrained False".format(
             backbone.kwargs.model_name
         ))
+        raise
 
     model = timm.create_model(**backbone.kwargs)
     return model
