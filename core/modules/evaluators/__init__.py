99f170b126ccb968a18f82647ec1e9bc935c9767
felixfu520 <alanmathisonturing@163.com>
2021-12-24 17:07:13 +0800

    @ evaluator

--- core/modules/evaluators/__init__.py
+++ core/modules/evaluators/__init__.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Copyright (c) Megvii, Inc. and its affiliates.
 
+# -*- coding: utf-8 -*-
+# @Author:FelixFu
+# @Date: 2021.12.17
+# @GitHub:https://github.com/felixfu520
+# @Copy From:
 
+from .SegEvaluator import SegEvaluator
