788f7056753ce4748fcca1b6c9ae783f4fcdb0ae
felixfu520 <alanmathisonturing@163.com>
2021-12-24 17:12:15 +0800

    @ register schedulers

---
+++ core/modules/schedulers/__init__.py
@@ -0,0 +1,5 @@
+from .cos_lr import cos_lr
+from .multistep_lr import multistep_lr
+from .warm_cos_lr import warm_cos_lr
+from .yolox_warm_cos import yolox_warm_cos
+from .yolox_semi_warm_cos_lr import yolox_semi_warm_cos_lr
