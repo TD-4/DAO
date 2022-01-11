7868925ab40dff96e1ca46d4f918027eec4ea353
felixfu520 <alanmathisonturing@163.com>
2022-01-11 17:12:39 +0800

    @ tensor image to numpy image

--- core/modules/utils/__init__.py
+++ core/modules/utils/__init__.py
@@ -1,6 +1,6 @@
 from .metricCls import MeterClsTrain, MeterClsEval, plot_confusion_matrix
 from .metricSeg import MeterSegTrain, MeterSegEval
 from .helpers import set_trainable, initialize_weights
-
+from .visualize import denormalization
 
 
