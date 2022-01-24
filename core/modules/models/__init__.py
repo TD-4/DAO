1676ac1455c665131316136963b1bcb90cb5a1fb
f4396 <fafu@lusterinc.com>
2022-01-24 19:17:22 +0800

    @ add deeplabv3 2

--- core/modules/models/__init__.py
+++ core/modules/models/__init__.py
@@ -8,7 +8,7 @@ from .cls import TIMMC
 from .anomaly import PaDiM
 
 # segmentation
-from .seg import Unet, UnetPlusPlus, DeepLabV3, DeepLabV3Plus, PSPNet, PSPNet2
+from .seg import Unet, UnetPlusPlus, DeepLabV3, DeepLabV3Plus, PSPNet, PSPNet2, DeepLabV3Plus2
 
 # iqa
 from .iqa import HyperNet
