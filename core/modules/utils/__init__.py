8388a08ea610234e1c547e40609300f2ca401edd
f4396 <fafu@lusterinc.com>
2022-01-26 09:46:47 +0800

    @ yolox boxes

--- core/modules/utils/__init__.py
+++ core/modules/utils/__init__.py
@@ -3,4 +3,14 @@ from .metricSeg import MeterSegTrain, MeterSegEval
 from .helpers import set_trainable, initialize_weights
 from .visualize import denormalization
 
+from .boxes import (
+    filter_box,
+    postprocess,
+    bboxes_iou,
+    matrix_iou,
+    adjust_box_anns,
+    xyxy2xywh,
+    xyxy2cxcywh,
+)
+
 
