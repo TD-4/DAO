0acc62ec5aa7f93ff1846448e1a26106db0b92dc
f4396 <fafu@lusterinc.com>
2022-01-27 16:28:30 +0800

    @ for package

--- core/version.py
+++ core/version.py
@@ -1,2 +1,6 @@
-
+__title__ = "DAO"
 __version__ = '0.0.1'
+__description__ = "first commit"
+__author__ = "felixfu"
+__author_email__ = "alanmathisonturing@163.com"
+__url__ = "https://github.com/FelixFu520/DAO"
