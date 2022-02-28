248772a94b5d199ff4e4ab66e405a295c33df49d
f4396 <fafu@lusterinc.com>
2022-02-28 18:33:54 +0800

    @ 主干网络TIMM，测试

--- core/modules/models/backbone/TIMM.py
+++ core/modules/models/backbone/TIMM.py
@@ -13,6 +13,11 @@ __all__ = ['TIMM']
 
 @Registers.backbones.register
 def TIMM(backbone):
+    """
+    获取TIMM主干网络
+
+    backbone:dict backbone:{kwargs:{这里面是timm库创建model的参数}}
+    """
     # 判断model是否在timm支持列表中
     if backbone.kwargs.model_name not in timm.list_models():
         logger.error("timm {} not supported {}".format(
@@ -29,3 +34,29 @@ def TIMM(backbone):
 
     model = timm.create_model(**backbone.kwargs)
     return model
+
+
+if __name__ == "__main__":
+    from dotmap import DotMap
+    from core.modules.register import Registers
+
+    model_c = {
+        "type": "TIMMC",
+        "summary_size": [1, 224, 224],
+        "backbone": {
+            "kwargs": {
+                "model_name": "efficientnet_b0",
+                "pretrained": True,
+                "checkpoint_path": "",
+                "exportable": True,
+                "in_chans": 1,
+                "num_classes": 38
+            }
+        },
+        "kwargs": {
+        }
+    }
+    model_c = DotMap(model_c)
+
+    model = Registers.backbones.get("TIMM")(model_c.backbone)
+    pass
